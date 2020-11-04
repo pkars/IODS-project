31fdc78401374bb18c4ae5b03c1ecd037e755bda
Petteri Karsisto <petteri.karsisto@fmi.fi>
2020-11-04 11:40:47 +0200

    Start data wrangling task 3

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -10,6 +10,7 @@
 # Year: 2020
 
 # Import external libraries
+library(dplyr)
 
 # Task 1: Create 'data' folder and a script (i.e. this file) [0pt]
 
@@ -25,3 +26,13 @@ head(full_data, 10)
 # 183 obs. of 60 variables. I.e. 183 rows and 60 columns of data points.
 # "gender" column contains strings, others are integers.
 str(full_data)
+
+# Task 3: Create analysis dataset [1pt]
+# Create combination variables as defined in metadata file
+# Deep approach = "Seeking Meaning" + "Relating Ideas" + "Use of Evidence"
+#   Seeking Meaning = D03 + D11 + D19 + D27
+#   Relating Ideas = D07 + D14 + D22 + D30
+#   Use of Evidence = D06 + D15 + D23 + D31
+# Note: All columns are of format "D\d\d", so we can use regular expression here
+deep <- select(full_data, matches(c("D\\d\\d"), perl = TRUE))
+head(deep, 4)
